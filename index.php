316f3c02362c49803c395198bb07644e4058468e
SergioIgnacioEstradaSimons <jeusslot555@gmail.com>
2026-09-12 01:44:23 +0000

    Configuracion Virtual Host

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupo 7 - Tecnología Web I</title>
+
+    <!-- Enlace correcto a la hoja de estilos CSS -->
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+
+    <div class="contenedor">
+
+        <h1>TECNOLOGÍA WEB I</h1>
+
+        <h2>GRUPO 7</h2>
+
+        <h3>Integrantes</h3>
+
+        <ul>
+            <li>Edilzon Jorge Katty Quispe</li>
+            <li>Ximena Calcina</li>
+            <li>Yamil Nieves</li>
+            <li>Sergio Estrada S.</li>
+        </ul>
+
+    </div>
+
+</body>
+
+</html>
